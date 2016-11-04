01e3bbfa708596367c888f4ce965dc14292bc046
chkumar <himakiran@gmail.com>
2016-11-04 17:08:53 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/chkappportfolio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.android.chkappportfolio;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        // Do something in response to button click
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_container));
+
+        TextView text = (TextView) layout.findViewById(R.id.text);
+        text.setText("This button will launch my " + buttonText + " App");
+        text.setTextSize(24);
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+    }
+}
